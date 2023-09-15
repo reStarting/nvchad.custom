aeceb04fb91202bec5dd2c79b578f3ed54316184
米世新 <sxmi01@wisedu.com>
2023-09-15 14:48:47 +0800

    update format

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,25 +1,28 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-  return
+	return
 end
 
 local b = null_ls.builtins
+local formatting = b.formatting
+local lint = b.diagnostics
 
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+	-- webdev stuff
+	formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	formatting.prettier, -- so prettier works only on these filetypes
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	formatting.clang_format,
+	lint.shellcheck,
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
--- init.lua
+++ init.lua
@@ -9,55 +9,50 @@ vim.g.copilot_tab_fallback = ""
 --   command = "tabdo wincmd =",
 -- })
 autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 autocmd({ "VimEnter" }, {
-  callback = function(data)
-    local dir = vim.fn.isdirectory(data.file) == 1
-    if not dir then
-      return
-    end
-    vim.cmd.cd(data.file)
-    require("nvim-tree.api").tree.open()
-  end,
+	callback = function(data)
+		local dir = vim.fn.isdirectory(data.file) == 1
+		if not dir then
+			return
+		end
+		vim.cmd.cd(data.file)
+		require("nvim-tree.api").tree.open()
+	end,
 })
 -- go to last loc when opening a buffer
 autocmd("BufReadPost", {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
-autocmd("BufWritePost", {
-               callback = function()
-    vim.lsp.buf.format()
-  end,
+autocmd("BufWritePre", {
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
 autocmd("FileType", {
-  pattern = {
-    "qf",
-    "help",
-    "man",
-    "notify",
-    "lspinfo",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "PlenaryTestPopup",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set(
-      "n",
-      "q",
-      "<cmd>close<cr>",
-      { buffer = event.buf, silent = true }
-    )
-  end,
+	pattern = {
+		"qf",
+		"help",
+		"man",
+		"notify",
+		"lspinfo",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"PlenaryTestPopup",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
 })
