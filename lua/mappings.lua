dc4ee2b4f8481a7fad77f8657929cb1fc2fb7107
米世新 <sxmi01@wisedu.com>
2024-08-15 13:56:45 +0800

    fix tmux nav

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,16 @@
 require "nvchad.mappings"
 
 -- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+map("n", "<c-h>", "<cmd>:TmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>:TmuxNavigateRight<cr>")
+map("n", "<c-\\>", "<cmd>:TmuxNavigatePrevious<cr>")
 
 map("n", "gw", "*N", { desc = "Find words" })
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
